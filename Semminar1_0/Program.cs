0fee3bdf8f1e3b3f66cdb81084e7dfeed18ef6ee
Elena <ees092@mail.ru>
2022-04-14 00:19:44 +0300

    Semminar 1 0

---
+++ Semminar1_0/Program.cs
@@ -0,0 +1,8 @@
+﻿int number, numberSquare;
+number = -10;
+numberSquare = number * number;
+Console.WriteLine($"Квадрат {number} равен {numberSquare}");
+
+number = 69;
+numberSquare = number * number;
+Console.WriteLine($"Квадрат {number} равен {numberSquare}");

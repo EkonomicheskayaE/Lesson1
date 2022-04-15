6550cc602ac60dec7317724ff041cdcfd3d0f2ed
Elena <ees092@mail.ru>
2022-04-15 23:37:52 +0300

    Homework 1 4

---
+++ Homework1_4/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Нахождение наибольшего из трёх чисел");
+double numberA = 2;
+double numberB = 3;
+double numberC = 7;
+double max = 0;
+if ((numberA > numberB) && (numberA > numberC))
+max = numberA;
+else if((numberB > numberA) && (numberB > numberC))
+max = numberB;
+else if((numberC > numberA) && (numberC > numberB))
+max = numberC;
+Console.WriteLine("Наибольшее из трёх чисел = " + max);
+Console.WriteLine();
+
+Console.WriteLine("Нахождение наибольшего из трёх чисел");
+double A = 44;
+double B = 5;
+double C = 78;
+double maxy = 0;
+if ((A > B) && (A > C))
+maxy = A;
+else if((B > A) && (B > C))
+maxy = B;
+else if((C > A) && (C > B))
+maxy = C;
+Console.WriteLine("Наибольшее из трёх чисел = " + maxy);
+Console.WriteLine();
+
+Console.WriteLine("Нахождение наибольшего из трёх чисел");
+double A1 = 22;
+double B2 = 3;
+double C3 = 9;
+double maxi = 0;
+if ((A1 > B2) && (A1 > C3))
+maxi = A1;
+else if((B2 > A1) && (B2 > C3))
+maxi = B2;
+else if((C3 > A1) && (C3 > B2))
+maxi = C3;
+Console.WriteLine("Наибольшее из трёх чисел = " + maxi);
+Console.WriteLine();

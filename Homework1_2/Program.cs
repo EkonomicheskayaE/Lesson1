5fe0a4019bab38430dd78b09a923ccb90a6b4ce9
Elena <ees092@mail.ru>
2022-04-15 23:45:20 +0300

    Homework 1 2

--- Homework1_2/Program.cs
+++ Homework1_2/Program.cs
@@ -1,11 +1,38 @@
-﻿int numberA, numberB;
+﻿Console.WriteLine("Найти максимальное число:");
+int numberA, numberB;
 numberA = 5;
 numberB = 7;
 if (numberA > numberB)
 {
-    Console.Write(numberA);
+    Console.WriteLine(numberA);
 }
 else
 {
-    Console.Write(numberB);
+    Console.WriteLine(numberB);
+}
+
+Console.WriteLine("Найти максимальное число:");
+int A, B;
+A = 2;
+B = 10;
+if (A > B)
+{
+    Console.WriteLine(A);
+}
+else
+{
+    Console.WriteLine(B);
+}
+
+Console.WriteLine("Найти максимальное число:");
+int X, Y;
+X = -9;
+Y = -3;
+if (X > Y)
+{
+    Console.WriteLine(X);
+}
+else
+{
+    Console.WriteLine(Y);
 }

56d93ac4e6b3af53fe5a7039efeb1969581c0759
Elena <ees092@mail.ru>
2022-04-16 00:14:35 +0300

    Homework 1 6

---
+++ Homework1_6/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Является ли число 4 четным?");
+int N;
+N = 4;
+
+if (N % 2==0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+}
+
+Console.WriteLine("Является ли число -3 четным?");
+int number1;
+number1 = -3;
+
+if (number1 % 2==0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+}
+
+Console.WriteLine("Является ли число 7 четным?");
+int number2;
+number2 = 7;
+
+if (number2 % 2==0)
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число не является чётным");
+}

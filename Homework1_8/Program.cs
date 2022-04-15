f73b6029369df603be76c6419738647b0813f431
Elena <ees092@mail.ru>
2022-04-16 00:59:34 +0300

    Homework 1 8

---
+++ Homework1_8/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Все чётные числа из диапозона от 1 до 5");
+int N, i;
+N = 5;
+i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    Console.WriteLine(i);
+    i++;
+}
+Console.WriteLine("Все чётные числа из диапозона от 1 до 8");
+int numberN, k;
+numberN = 8;
+k = 1;
+while (k <= numberN)
+{
+    if (k % 2 == 0)
+    Console.WriteLine(k);
+    k++;
+}
+

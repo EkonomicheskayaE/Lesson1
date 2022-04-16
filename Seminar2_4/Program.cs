f16ddb632a09184d62739a56c77b4ee2987f184a
Elena <ees092@mail.ru>
2022-04-16 12:28:58 +0300

    Seminar 2 4

---
+++ Seminar2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int result = numberA / numberB;
+
+if (numberA % numberB == 0)
+{
+    Console.WriteLine($"Кратно, результат: {result}");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток от деления: {numberA % numberB}");
+}

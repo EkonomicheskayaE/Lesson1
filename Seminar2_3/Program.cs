5331f868b4aedf44a39cc194f7c1a1e4d2d1e5dd
Elena <ees092@mail.ru>
2022-04-16 12:27:57 +0300

    Seminar 2 3

---
+++ Seminar2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// int numberA = new Random().Next(1, 10);
+int numberA = new Random().Next(10, 100);
+Console.WriteLine(numberA);
+
+int b = numberA / 10;
+int c = numberA % 10;
+
+if (b > c)
+{
+    Console.WriteLine($"большее число {b}");
+}
+else
+{
+    Console.WriteLine($"наибольшая цифра {c}");
+}

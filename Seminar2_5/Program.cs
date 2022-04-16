8529437b83e5d3d795b579db5a9e396efff7743f
Elena <ees092@mail.ru>
2022-04-16 12:29:49 +0300

    Seminar 2 5

---
+++ Seminar2_5/Program.cs
@@ -0,0 +1,16 @@
+﻿// остаток от деления
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = 7;
+int numberC = 23;
+
+if ((numberA % numberB == 0) & (numberA % numberC == 0))
+{
+    Console.WriteLine($"Кратно {numberB} и {numberC}");
+}
+else
+{
+    Console.WriteLine($"Не кратно {numberB} и {numberC}");
+}

ab1a35b6cfe92f5fb5bee11c59cfb2acdef2bbc7
Elena <ees092@mail.ru>
2022-04-17 11:59:50 +0300

    Seminar 3 1

---
+++ Seminar3_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Определение координаты на плоскости
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x>0) & (y>0))
+{
+    Console.WriteLine("точка в 1-й четверти");
+}
+if ((x>0) & (y<0))
+{
+    Console.WriteLine("точка в 4-й четверти");
+}
+if ((x<0) & (y>0))
+{
+    Console.WriteLine("точка в 2-й четверти");
+}
+if ((x<0) & (y<0))
+{
+    Console.WriteLine("точка в 3-й четверти");
+}
+if (x == 0 | y == 0)
+{
+    Console.WriteLine("ошибка");
+}

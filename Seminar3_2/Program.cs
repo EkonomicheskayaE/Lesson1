35b2181a7f225339cee39f72a1814e66e90c73df
Elena <ees092@mail.ru>
2022-04-17 12:01:18 +0300

    Seminar 3 2

---
+++ Seminar3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// определяем четверть
+Console.Clear();
+int r = new Random().Next (1,4);
+Console.WriteLine($"r = {r}");
+// int с = Convert.ToInt32(Console.ReadLine());
+
+if (r == 1)
+{
+    Console.WriteLine("x > 0 b y > 0");
+}
+else if (r == 2)
+{
+    Console.WriteLine("x > 0 b y < 0");
+}
+else if (r == 3)
+{
+    Console.WriteLine("x < 0 b y < 0");
+}
+else if (r == 4)
+{
+    Console.WriteLine("x > 0 b y < 0");
+}
+else
+{
+    Console.WriteLine("ошибка, нет такой четверти");
+}

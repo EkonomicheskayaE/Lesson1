2bc903f96154f73243e0065c754736123b23a4dc
Elena <ees092@mail.ru>
2022-04-17 12:08:02 +0300

    Seminar 3 4

---
+++ Seminar3_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// квадраты чисел
+Console.WriteLine("Введите любое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N <= 0)
+{
+    N = -N;
+    Console.WriteLine("введите новое число:");
+}
+else
+{
+   int numbers = 1;
+
+Console.WriteLine("Все квадраты чисел с одного по заданное число: ");
+
+while (numbers <= N)
+{
+    Console.WriteLine(Math.Pow(numbers,2));
+    numbers++;
+}
+}
+
+int n = 9;
+int i = 1;
+while (i < n)
+{
+    Console.Write($"{i*i}, ");
+    i++;
+}
+Console.WriteLine($"{i*i}. ");

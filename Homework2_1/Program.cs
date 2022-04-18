463dc763e6fec6f07a9071a34c7b3be8efcb168f
Elena <ees092@mail.ru>
2022-04-18 23:54:55 +0300

    Homework 2 1

---
+++ Homework2_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа, которая на вход принимает 3ёх значное число и показывает 2ую цифру этого числа.
+Console.Clear();
+Console.Write("Введите 3ёх значное число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int D = M/10;
+int O = D%10;
+Console.WriteLine($"Вторая цифра равна {O}");

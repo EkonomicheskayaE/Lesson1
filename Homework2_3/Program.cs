1e428509bc20ad8152e7325dc4b339d942e55fe6
Elena <ees092@mail.ru>
2022-04-18 23:56:51 +0300

    Homework 2 3

---
+++ Homework2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Третья цифра заданного числа, если есть.
+Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+if (X < 100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else if (X > 99)
+{
+    Console.WriteLine(X.ToString()[2]);
+}
+    

51a9448e8309ebdafbccabba790b64a6e3ea2f83
Elena <ees092@mail.ru>
2022-04-18 23:55:56 +0300

    Homework 2 2

---
+++ Homework2_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Выводим 3ёх значное число и удаляем вторую цифру.
+Console.Clear();
+int sum = new Random().Next(100, 999);
+Console.WriteLine($"sum = {sum}");
+int C1 = sum/100;
+int C3 = sum%10;
+Console.Write($"Удаляем вторую цифру и получаем {C1}{C3}");

71f3e558b8333c9c22b97a08600b0028c8b2dd46
Elena <ees092@mail.ru>
2022-04-21 23:45:37 +0300

    Homework 3 23

---
+++ Homework3_23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Таблица кубов.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int K = Convert.ToInt32(Console.ReadLine());
+
+if (K<0)
+{
+    K = -K;
+    Console.WriteLine("Введите другое число: ");
+}
+else
+{
+    int mean = 1;
+    Console.WriteLine("Кубы чисел: ");
+
+while (mean <= K)
+{
+   Console.WriteLine(Math.Pow(mean,3));
+   mean++;
+}
+}

afeced1c28829e2d9e29058f80771ee3e3774470
Elena <ees092@mail.ru>
2022-04-21 23:41:44 +0300

    Homework 3 19

---
+++ Homework3_19/Program.cs
@@ -0,0 +1,21 @@
+﻿// Проверка пятизначного числа на полиндромность.
+Console.Clear();
+Console.WriteLine("Введите любое пятизначное число: ");
+int F = Convert.ToInt32(Console.ReadLine());
+int temp = F;
+int rev = 0;
+int dig;
+while (F>0)
+{
+    dig = F%10;
+    rev = rev*10+dig;
+    F = F/10;
+}
+if (temp == rev)
+{
+    Console.WriteLine("Число является плиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является плиндромом");
+}
